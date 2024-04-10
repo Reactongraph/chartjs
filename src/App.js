0ad59b157e03ae6d32085dd52c11fb81cf263253
Reactongraph <js.developer@ongraph.com>
2024-04-10 18:54:41 +0530

    fix backend api bug

--- src/App.js
+++ src/App.js
@@ -1,37 +1,28 @@
-import React from 'react';
-
-import Cards from './components/Cards/Cards';
-import Chart from './components/Chart/Chart';
-import CountryPicker from './components/CountryPicker/CountryPicker';
-import styles from './App.module.css';
-import { fetchData } from './api';
-
+import React from "react";
+import Cards from "./components/Cards/Cards";
+import Chart from "./components/Chart/Chart";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
 
 class App extends React.Component {
-  state= {
-     data:{},
-     country:'',
+  state = {
+    data: {},
   };
-  
-   async componentDidMount() {
-        const fetchedData = await fetchData();
-       this.setState({data:fetchedData});
-    }
-    handleCountryChange = async (country) => {
-        const fetchedData = await fetchData(country);
-        console.log(fetchedData);
-       
-    }
-    render() {
-        const {data,country} = this.state;
-        return (
-            <div className={styles.container}>
-                <Cards data={data} />
-                <CountryPicker handleCountryChange=  {this.handleCountryChange} />
-                <Chart data={data} country={country} />
-            </div>
-        );
-    }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
 }
 
 export default App;
